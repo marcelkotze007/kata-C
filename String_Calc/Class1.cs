8a4d6d3500b867f5872a041ad0ffcc94cf32f8fe
Marcel Kotze <marcel.kotze@investec.co.za>
2021-10-26 15:57:56 +0200

    feat: completed first 6 tests

--- String_Calc/Class1.cs
+++ String_Calc/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace String_Calc
 {
@@ -7,6 +8,36 @@ namespace String_Calc
         public static int Add(string data)
         {
             int answer = 0;
+            // string neg_answer = "negatives not allowed";
+            string[] values;
+            var delim_list = new List<string>()
+                {",","\n"};
+
+            if (data == "")
+            {
+                return 0;
+            }
+
+            if (data.Contains("//"))
+            {
+                int delim_pos = data.IndexOf("\n");
+                string delim = data.Substring(2,delim_pos-2);
+                delim_list.Add(delim);
+                data = data.Substring(delim_pos+1);
+            }
+
+            foreach (var delim in delim_list)
+            {
+                data = data.Replace(delim, delim_list[0]);
+            }
+
+            values = data.Split(",");
+
+            foreach (var item in values)    
+            {
+                answer = answer + Convert.ToInt32(item);
+            }
+
             return answer;
         }
     }
--- String_Calc_test/UnitTest1.cs
+++ String_Calc_test/UnitTest1.cs
@@ -7,9 +7,39 @@ namespace String_Calc_test
     public class UnitTest1
     {
         [Fact]
-        public void test_empty()
+        public void Tests_Zero()
         {
             Assert.Equal(0, Class1.Add(""));
+        }
+
+        [Fact]
+        public void Tests_One()
+        {
+            Assert.Equal(1, Class1.Add("1"));
+        }
+
+        [Fact]
+        public void Tests_Two()
+        {
+            Assert.Equal(3, Class1.Add("1,2"));
+        }
+
+        [Fact]
+        public void Tests_Unkown()
+        {
+            Assert.Equal(10, Class1.Add("1,2,3,4"));
+        }
+
+        [Fact]
+        public void Tests_Newline()
+        {
+            Assert.Equal(10, Class1.Add("1\n2,3,4"));
+        }
+
+        [Fact]
+        public void Tests_User_Delim()
+        {
+            Assert.Equal(3, Class1.Add("//;\n1;2"));
         }
     }
 }
